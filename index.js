6559bf9de180b19b23769d50348a63dee75bc8ef
zuiqingri <72846494+zuiqingri@users.noreply.github.com>
2021-10-15 21:17:59 +0800

    Hiden secret

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser=require('body-parser');
+
+const config=require('./config/key');
+
+const {User}=require("./models/User");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 const mongoose=require('mongoose')
 // mongoose.connect('mongodb://localhost:27017/example?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false',{
 //     useNewUrlParser:true,useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
 // }).then(()=>console.log('MongoDB Connected...'))
 //    .catch(err=>console.log(err))
-   mongoose.connect('mongodb://localhost:27017/example?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false')
+   mongoose.connect(config.mongoURI)
    .then(()=>console.log('MongoDB Connected...'))
    .catch(err=>console.log(err))
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register',(req,res)=>{
+const user=new User(req.body)
+user.save((err,userInfo)=>{
+    if(err) return res.json({success:false,err})
+    return res.status(200).json({
+        success:true
+    })
+})
 })
 
 app.listen(port, () => {
