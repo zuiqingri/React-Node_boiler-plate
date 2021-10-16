e46ff9af9f8a81ad2c4338f2b549772e67185c65
zuiqingri <72846494+zuiqingri@users.noreply.github.com>
2021-10-16 08:06:13 +0800

    making token,auth

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const mongoose=require('mongoose');
-
+const bcrypt=require('bcrypt');
+const saltRounds=10;
+const jwt=require('jsonwebtoken');
 const userSchema=mongoose.Schema({
     name:{
         type:String,
@@ -31,6 +33,57 @@ const userSchema=mongoose.Schema({
     }
 
 })
+
+userSchema.pre('save',function(next){
+    var user=this;
+
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+                // Store hash in your password DB.
+                user.password=hash
+                next()
+            });
+        });
+    } else{
+        next();
+    }
+    
+})
+
+userSchema.methods.comparePassword=function(plainPassword,cb){
+
+    bcrypt.compare(plainPassword,this.password,function(err,isMatch){
+        if(err) return cb(err),
+        cb(null,isMatch)
+    })
+}
+
+userSchema.methods.generateToken=function(cb){
+
+    var user=this;
+    //console.log('user._id',user._id);
+    var token=jwt.sign(user._id.toHexString(),'secretToken')
+
+    user.token=token
+    user.save(function(err,user){
+        if(err) return cb(err)
+        cb(null,user)
+    })
+}
+
+userSchema.statics.findByToken=function(token,cb){
+    var user=this;
+    jwt.verify(token,'secretToken',function(err,decoded){
+
+    user.findOne({"_id":decoded,"token":token},function(err,user){
+        if(err) return cb(err);
+        cb(null,user)
+    })
+  })
+}
 
 const User=mongoose.model('User',userSchema)
 
